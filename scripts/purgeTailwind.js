baacc2f53556e82f9b244da61d3f8ca2eac4f13d
Tona González <martiuh@gmail.com>
2020-10-11 18:09:01 -0500

    Add postbuild script

---
+++ scripts/purgeTailwind.js
@@ -0,0 +1,33 @@
+const { PurgeCSS } = require('purgecss');
+const fs = require('fs');
+const clientStats = require('../artifacts/client-stats.json');
+
+async function purgeTailwindMainCss() {
+  process.chdir('./scripts');
+  const { assetsByChunkName } = clientStats;
+  const mainCssFile = assetsByChunkName['main'].find(files =>
+    files.endsWith('.css')
+  );
+
+  if (!mainCssFile) {
+    console.warn('[purgeTailwind] No main css file found');
+    return;
+  }
+
+  const purgeCSSResult = await new PurgeCSS().purge({
+    content: ['../dist/**/*.html'],
+    css: [`../dist/${mainCssFile}`]
+  });
+
+  const [purgedMainCss] = purgeCSSResult;
+
+  if (purgedMainCss) {
+    fs.writeFile(`../dist/${mainCssFile}`, purgedMainCss.css, err => {
+      if (err) {
+        console.error("Couldn't create main file");
+      }
+    });
+  }
+}
+
+purgeTailwindMainCss();
