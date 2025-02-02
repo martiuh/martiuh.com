51ccde59b3bacbeddfc7691c70adfaa20984c489
Tona González <martiuh@gmail.com>
2025-02-02 08:05:11 -0600

    Add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [react()],
   output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
