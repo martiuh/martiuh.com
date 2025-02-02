db85d806bbf977972ea2f82de6066d7e2e60824d
Tona González <martiuh@gmail.com>
2025-02-02 08:11:35 -0600

    use netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()],
   output: "server",
-
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
 });
